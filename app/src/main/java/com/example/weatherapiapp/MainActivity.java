49b7a93062703a31cfd7a22b3d5980400bf318e0
undencem <undencem@gmail.com>
2021-02-10 23:42:06 +0300

    Initial commit

--- app/src/main/java/com/example/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapiapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -22,15 +23,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity  {
 
     private static final String TAG = "MainActivity";
     
-    Button btn_cityID,btn_WeatherByCity,btn_WeatherByCityName;
+    Button btn_cityID,btn_getWeatherByID,btn_WeatherByCityName;
     EditText et_datainput;
     ListView lv_weatherReport;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity  {
 
         //assign values to each control in the layout
         btn_cityID = (Button)findViewById(R.id.btn_cityID);
-        btn_WeatherByCity=(Button)findViewById(R.id.btn_WeatherByCityID);
+        btn_getWeatherByID=(Button)findViewById(R.id.btn_WeatherByCityID);
         btn_WeatherByCityName=(Button)findViewById(R.id.btn_WeatherByCityName);
         et_datainput = (EditText)findViewById(R.id.et_datainput);
         lv_weatherReport=(ListView)findViewById(R.id.lv_weatherReports);
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View v) {
 
 
-                //Didnt return anything
+                //Didn't return anything
                  weatherDataService.getCityID(et_datainput.getText().toString(), new WeatherDataService.VolleyResponseListener() {
                     @Override
                     public void onError(String message) {
@@ -67,17 +68,37 @@ public class MainActivity extends AppCompatActivity  {
 
                     @Override
                     public void onResponse(String cityID) {
+                        Log.d(TAG, "onResponse: getCityID ");
                         Toast.makeText(MainActivity.this,"Returned as ID of:"+cityID,Toast.LENGTH_LONG).show();
                     }
                 });
             }
         });
 
-       btn_WeatherByCity.setOnClickListener(new View.OnClickListener(){
+
+       btn_getWeatherByID.setOnClickListener(new View.OnClickListener(){
            @Override
-           public void onClick(View v){
-               Log.e(TAG, "onClick: btn_WeatherByCity" );
-                Toast.makeText(getApplicationContext(),"You clicked 2",Toast.LENGTH_LONG).show();
+           public void onClick(View v) {
+
+               weatherDataService.getCityForecastByID(et_datainput.getText().toString(), new WeatherDataService.ForeCastByIDResponse() {
+
+                   @Override
+                   public void onError(String message) {
+                       Log.e(TAG, "onError: getCityForecastByID in MainActivity");
+                       Toast.makeText(MainActivity.this,"Error",Toast.LENGTH_SHORT).show();
+                   }
+
+                   @Override
+                   public void onResponse(List<WeatherReportModel> weatherReportModel) {
+
+                       // put the entire list into ListView control
+                       Log.d(TAG, "onResponse: getWeatherByID");
+                       ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,weatherReportModel);
+
+                       // assign arrayAdapter to ListView
+                       lv_weatherReport.setAdapter(arrayAdapter);
+                   }
+               });
            }
        });
 
--- app/src/main/java/com/example/weatherapiapp/WeatherDataService.java
+++ app/src/main/java/com/example/weatherapiapp/WeatherDataService.java
@@ -7,17 +7,19 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherDataService {
 
     public static final String QUERY_FOR_CITY_ID = "https://www.metaweather.com/api/location/search/?query=";
-
+    public static final String QUERY_FOR_CITY_WEATHER_ID = "https://www.metaweather.com/api/location/";
     Context context;
     String cityID;
 
@@ -68,9 +70,88 @@ public class WeatherDataService {
 
     };
 
-    public List<WeatherReportModel> getCityForecastByID(String cityID){
-        return null;
+    public interface ForeCastByIDResponse {
+        void onError(String message);
+
+        void onResponse(List<WeatherReportModel> weatherReportModel);
+    }
+
+    public void  getCityForecastByID(String cityID,ForeCastByIDResponse foreCastByIDResponse){
+
+        List<WeatherReportModel> weatherReportModels = new ArrayList<WeatherReportModel>() ;
+
+        String url = QUERY_FOR_CITY_WEATHER_ID+cityID;
+
+        // get the json object
+        JsonObjectRequest request =
+                new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                     public void onResponse(JSONObject response ) {
+
+                             try {
+                                    JSONArray consolidated_weather_list = response.getJSONArray("consolidated_weather");
+
+                                    for( int i=0 ; i < consolidated_weather_list.length() ; i++ ){
+
+                                         // get the first item in the array
+                                         WeatherReportModel one_day_weather = new WeatherReportModel();
+
+                                         // get the property called "consolidated weather" which is array
+                                         // get each item in the array and assign it to a new WeatherReportModel object
+
+                                         JSONObject first_day_from_api =(JSONObject) consolidated_weather_list.get(i);
+
+                                         one_day_weather.setId(first_day_from_api.getInt("id"));
+
+                                         one_day_weather.setWeather_state_name(first_day_from_api.getString("weather_state_name"));
+
+                                         one_day_weather.setWeather_state_abbr(first_day_from_api.getString("weather_state_abbr"));
+
+                                         one_day_weather.setWind_direction_compass(first_day_from_api.getString("wind_direction_compass"));
+
+                                         one_day_weather.setCreated(first_day_from_api.getString("created"));
+
+                                         one_day_weather.setApplicable_date(first_day_from_api.getString("applicable_date"));
+
+                                         one_day_weather.setMin_temp(first_day_from_api.getLong("min_temp"));
+
+                                         one_day_weather.setMax_temp(first_day_from_api.getLong("max_temp"));
+
+                                         one_day_weather.setThe_temp(first_day_from_api.getLong("the_temp"));
+
+                                         one_day_weather.setWind_speed(first_day_from_api.getLong("wind_speed"));
+
+                                         one_day_weather.setWind_direction(first_day_from_api.getLong("wind_direction"));
+
+                                         one_day_weather.setAir_pressure(first_day_from_api.getInt("air_pressure"));
+
+                                         one_day_weather.setHumidity(first_day_from_api.getInt("humidity"));
+
+                                         one_day_weather.setVisibility(first_day_from_api.getLong("visibility"));
+
+                                         one_day_weather.setPredictability(first_day_from_api.getInt("predictability"));
+
+
+                                         weatherReportModels.add(one_day_weather);
+                                     }
+
+                                foreCastByIDResponse.onResponse(weatherReportModels);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                foreCastByIDResponse.onError("Error");
+            }
+        });
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
     };
+
 
     public List<WeatherReportModel> getCityForecastByName(String cityName){
         return null;
